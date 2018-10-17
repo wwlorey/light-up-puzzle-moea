322a84d61ec3702cfc8a7d82e8f41270bfbabebf
William Lorey <wwlorey@gmail.com>
2018-10-17 11:02:22 -0500

    Update log file paths for analysis

--- analysis/analysis_config.py
+++ analysis/analysis_config.py
@@ -1,18 +1,13 @@
 log_file_paths = [
-                    'random_gen/random_gen_uniform_random',
-                    'random_gen/random_gen_validity_enforced', 
-                    'random_gen_bonus/random_gen_uniform_random_bonus',
-                    'random_gen_bonus/random_gen_validity_enforced_bonus',
-                    'random_gen_vanilla/random_gen_uniform_random_vanilla',
-                    'random_gen_vanilla/random_gen_validity_enforced_vanilla',
-                    'website_puzzle/website_puzzle_uniform_random', 
-                    'website_puzzle/website_puzzle_validity_enforced', 
-                    'website_puzzle_bonus/website_puzzle_uniform_random_bonus',
-                    'website_puzzle_bonus/website_puzzle_validity_enforced_bonus',
-                    'website_puzzle_vanilla/website_puzzle_uniform_random_vanilla',
-                    'website_puzzle_vanilla/website_puzzle_validity_enforced_vanilla',
-                    'website_puzzle/website_puzzle_validity_enforced_small_penalty',
-                    'website_puzzle/website_puzzle_validity_enforced_large_penalty'
+                    'random_gen/random_gen__fitness_proportional_parent__fitness_proportional_survival__comma_',
+                    'random_gen/random_gen__tournament_parent__fitness_proportional_survival__comma_',
+                    'random_gen/random_gen__fitness_proportional_parent__fitness_proportional_survival__plus_',
+                    'random_gen/random_gen__tournament_parent__fitness_proportional_survival__plus_',
+                    'random_gen/random_gen__fitness_proportional_parent__tournament_survival__comma_',
+                    'random_gen/random_gen__tournament_parent__tournament_survival__comma_',
+                    'random_gen/random_gen__fitness_proportional_parent__tournament_survival__plus_',
+                    'random_gen/random_gen__tournament_parent__tournament_survival__plus_',
+                    'random_gen/random_gen_uniform_random_init'
                  ]
 
 
