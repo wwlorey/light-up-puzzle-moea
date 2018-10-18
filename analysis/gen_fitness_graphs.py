9db51e0422a57cb02b00de9697898b4c7b28fdc2
William Lorey <wwlorey@gmail.com>
2018-10-18 12:01:10 -0500

    Add graphs for first three subfitnesses

--- analysis/gen_fitness_graphs.py
+++ analysis/gen_fitness_graphs.py
@@ -8,7 +8,7 @@ import numpy as np
 
 for q in range(len(config.log_file_paths)):
     with open(config.log_file_paths[q], 'r') as log_file:
-        # Create a list of lines from the log file, disregarding all config parmeters and empty lines
+        # Create a list of lines from the log file, disregarding all config parameters and empty lines
         log_file = log_file.read().split('\n')
         log_file = [line for line in log_file[log_file.index('Run 1'):] if not line == '']
 
@@ -22,61 +22,87 @@ for q in range(len(config.log_file_paths)):
         max_num_evals = 0
         for line in log_file:
             if not line[0] == 'R':
-                # This line has eval and fitness data
-                eval_num, avg_fit, best_fit = line.split('\t')
+                # This line has data
+                eval_num, avg_shine_ratio, best_shine_ratio, avg_bulb_constr, best_bulb_constr, \
+                    avg_black_cell_constr, best_black_cell_constr = line.split('\t')
 
                 eval_num = int(eval_num)
-                avg_fit = float(avg_fit)
-                best_fit = float(best_fit)
+                avg_shine_ratio = float(avg_shine_ratio)
+                best_shine_ratio = float(best_shine_ratio)
+                avg_bulb_constr = float(avg_bulb_constr)
+                best_bulb_constr = int(best_bulb_constr)
+                avg_black_cell_constr = float(avg_black_cell_constr)
+                best_black_cell_constr = int(best_black_cell_constr)
 
                 if eval_num in eval_dict:
-                    eval_dict[eval_num][0] += avg_fit
-                    eval_dict[eval_num][1] += best_fit
-                    eval_dict[eval_num][2] += 1
+                    eval_dict[eval_num][0] += avg_shine_ratio
+                    eval_dict[eval_num][1] += best_shine_ratio
+                    eval_dict[eval_num][2] += avg_bulb_constr
+                    eval_dict[eval_num][3] += best_bulb_constr
+                    eval_dict[eval_num][4] += avg_black_cell_constr
+                    eval_dict[eval_num][5] += best_black_cell_constr
+                    eval_dict[eval_num][6] += 1
                 else:
-                    eval_dict[eval_num] = [avg_fit, best_fit, 1]
+                    eval_dict[eval_num] = [avg_shine_ratio, best_shine_ratio, avg_bulb_constr,
+                        best_bulb_constr, avg_black_cell_constr, best_black_cell_constr, 1]
                 
-                max_num_evals = max(eval_dict[eval_num][2], max_num_evals)
+                max_num_evals = max(eval_dict[eval_num][6], max_num_evals)
 
         # Adjust the counts up to the largest count
+        '''
         for eval_num in eval_dict:
             eval_dict[eval_num][2] = max_num_evals
-
+        '''
 
         evals = []
-        avg_fits = []
-        best_fits = []
+        avg_shine_ratios = []
+        best_shine_ratios = []
+        avg_bulb_constrs = []
+        best_bulb_constrs = []
+        avg_black_cell_constrs = []
+        best_black_cell_constrs = []
+
         for eval_num in sorted(eval_dict.keys()):
             evals.append(eval_num)
-            avg_fits.append(eval_dict[eval_num][0] / eval_dict[eval_num][2])
-            best_fits.append(eval_dict[eval_num][1] / eval_dict[eval_num][2])
+            avg_shine_ratios.append(eval_dict[eval_num][0] / eval_dict[eval_num][6])
+            best_shine_ratios.append(eval_dict[eval_num][1] / eval_dict[eval_num][6])
+            avg_bulb_constrs.append(eval_dict[eval_num][2] / eval_dict[eval_num][6])
+            best_bulb_constrs.append(eval_dict[eval_num][3] / eval_dict[eval_num][6])
+            avg_black_cell_constrs.append(eval_dict[eval_num][4] / eval_dict[eval_num][6])
+            best_black_cell_constrs.append(eval_dict[eval_num][5] / eval_dict[eval_num][6])
+
+
+        # Configure graphing data
+        plot_data = \
+            [
+                (avg_shine_ratios, best_shine_ratios, "Lit Cell Ratio", "lit_cell_ratio", 1), 
+                (avg_bulb_constrs, best_bulb_constrs, 
+                    "Number of Bulb Shine Constraints Violated", "bulb_shine_constr", 2),
+                (avg_black_cell_constrs, best_black_cell_constrs, 
+                    "Number of Black Cell Constraints Violated", "black_cell_constr", 10)
+            ]
 
         # Plot the results
-        fig, ax = plt.subplots()
+        for plot_datum in plot_data:
 
-        ax.step(evals, avg_fits, '-r')
-        ax.step(evals, best_fits, '-b')
+            fig, ax = plt.subplots()
 
-        if 'bonus' in config.log_file_paths[q] or 'small' in config.log_file_paths[q]:
-            # These plots only include fitness values between 0 and 1
-            plt.ylim(0, 1)
-        elif 'large' in config.log_file_paths[q]:
-            # These plots include both negative and positive fitness values
-            plt.ylim(-2.5, 1)
-        else:
-            # These plots include both (smaLl) negative and positive fitness values
-            plt.ylim(-1, 1)
+            ax.step(evals[:len(plot_datum[0])], plot_datum[0], '-r')
+            ax.step(evals[:len(plot_datum[1])], plot_datum[1], '-b')
+    
+            plt.ylim(0, plot_datum[4])
+    
+            red_patch = mpatches.Patch(color='red', label='Average Local ' + plot_datum[2])
+            blue_patch = mpatches.Patch(color='blue', label='Local Best ' + plot_datum[2])
+            plt.legend(handles=[blue_patch, red_patch])
+    
+            # Include necessary labels
+            plt.xlabel('Evaluations')
+            plt.ylabel(plot_datum[2])
+    
+    
+            # Save and close the plot
+            plt.savefig(config.log_file_paths[q][:config.log_file_paths[q].find('log')] + 
+                plot_datum[3] + '__graph.png')
+            plt.close()
 
-        red_patch = mpatches.Patch(color='red', label='Average Local Fitness')
-        blue_patch = mpatches.Patch(color='blue', label='Local Best Fitness')
-        plt.legend(handles=[blue_patch, red_patch])
-
-        # Include necessary labels
-        plt.xlabel('Evaluations')
-        plt.ylabel('Fitness (ratio of lit white cells to total number of white cells)')
-
-
-        # Save and close the plot
-        plt.savefig(config.log_file_paths[q][:config.log_file_paths[q].find('log')] + 'graph.png')
-        plt.close()
-            
